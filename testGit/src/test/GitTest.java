235498f4dd63679db329b5f4c78eb5482dae26ef
hayato0622 <hayato06222@gmail.com>
2026-07-22 14:08:53 +0900

    GitTest.javaに一文追加

--- testGit/src/test/GitTest.java
+++ testGit/src/test/GitTest.java
@@ -5,5 +5,7 @@ public class GitTest {
 		System.out.println("Hello GitHub!!");
 		System.out.println("Good Morning GitHub!!");
 		System.out.println("Good Night GitHub!!");
+		System.out.println("Good bye GitHub!!");
+
 	}
 }
